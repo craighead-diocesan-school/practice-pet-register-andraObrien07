48e45f50d3e7b9d34cf1a58cb5e2c19e440734b0
andraObrien07 <comanan09@craighead.school.nz>
2023-07-24 11:28:00 +1200

    Added an array of pets
    to then be able to added more pets to that array through the prompt of 'nameAdd' and also added another function that alows the user to see the pet names that are already in the arrary

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+let PetArray = [
+    'Scruffy',
+    'Puffy',
+    'Fluffy',
+    'Ed',
+    'Keria',
+    'Zoro',
+    'Bear',
+    'Dolo',
+    'Flynn',
+    'Baa-Baa',
+    'Blinky',
+    'Barky',
+    'Pinky-Nose',
+    'Rosey-Petals',
+]
+
+
+
+function PetRegister() {
+    let nameAdd = prompt('What is the name of the pet you want to add')
+
+    while (nameAdd != 'stop') {
+
+        subjects.push(newSubject)
+
+
+    }
+}
+
+
+
+
+function ViewPets() {
+    alert(PetArray)
+
+}
